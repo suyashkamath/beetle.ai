36add01d0862d540007b1e555a1e9218e22e2092
adil-khursheed <adilkhursheed60@gmail.com>
2025-11-10 19:23:47 +0530

    fix: props error in upgradePlanCard component

--- apps/web/app/(root)/dashboard/_components/UpgradePlanCard.tsx
+++ apps/web/app/(root)/dashboard/_components/UpgradePlanCard.tsx
@@ -1,31 +1,30 @@
 "use client";
 
-import React, { useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 import { UpgradePlanDialog } from "@/components/shared/UpgradePlanDialog";
-import { TrendingUp } from "lucide-react";
 
 const UpgradePlanCard = () => {
-  const [open, setOpen] = useState(false);
-
   return (
-    <Card className="relative overflow-hidden mx-auto mb-8 p-5 w-full">
-      <CardHeader className="p-0 ">
-        <CardTitle className="text-xl">You are currently using free plan</CardTitle>
+    <Card className="relative mx-auto mb-8 w-full overflow-hidden p-5">
+      <CardHeader className="p-0">
+        <CardTitle className="text-xl">
+          You are currently using free plan
+        </CardTitle>
         <CardDescription className="text-sm">
-          If you are startup/company or team, you can upgrade your plan for free to access teams feature and increase limits for analyses.
+          If you are startup/company or team, you can upgrade your plan for free
+          to access teams feature and increase limits for analyses.
         </CardDescription>
       </CardHeader>
       <CardContent className="p-0">
-        <Button className="cursor-pointer" onClick={() => setOpen(true)}>
-                        <TrendingUp className="h-4 w-4" />
-
-          Upgrade for free
-        </Button>
+        <UpgradePlanDialog />
       </CardContent>
-
-      <UpgradePlanDialog open={open} onOpenChange={setOpen} />
     </Card>
   );
 };
